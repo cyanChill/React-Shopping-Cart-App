359b0e07dfb032bb55873b75f2240fb1689fef3e
cyanChill <anthonyliang9@gmail.com>
2022-01-06 21:27:42 -0500

    eventlistener to give nav bkg if we scroll from the start pos

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -1,10 +1,4 @@
-/* 
-  - Logo on left side of navbar
-  - 2 Buttons on right side of navbar:
-      1. Shopping Cart
-      2. Navigation Sidebar Clicker
-*/
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaShoppingCart, FaBars } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
@@ -14,6 +8,7 @@ import logo from "../../assets/images/ltt_logo.png";
 import classes from "./Navbar.module.css";
 
 const Navbar = () => {
+  const [blurNav, setBlurNav] = useState(false);
   const [show, setShow] = useState(false);
 
   const showSideBar = () => setShow(true);
@@ -23,12 +18,25 @@ const Navbar = () => {
     Need check to see if we scrolled on the page; if we have, need to give
     the navebar a "active class" (ie: make the background not transparent)
   */
+  useEffect(() => {
+    const scrollListener = window.addEventListener("scroll", () => {
+      if (window.scrollY === 0) {
+        setBlurNav(false);
+      } else {
+        setBlurNav(true);
+      }
+    });
+
+    return () => {
+      window.removeEventListener("scroll", scrollListener);
+    };
+  }, []);
 
   return (
     <>
       <Sidebar show={show} onClick={hideSideBar} />
 
-      <div className={classes.navbar}>
+      <div className={`${classes.navbar} ${blurNav && classes.blur}`}>
         <div className={classes.logo}>
           <Link to="/">
             <img src={logo} alt="Logo" />
