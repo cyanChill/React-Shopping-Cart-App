a7101a61285fee80363198bdb348d17b5a7c746b
cyanChill <anthonyliang9@gmail.com>
2022-01-09 17:38:22 -0500

    add option to have the loading spinner have a backdrop

--- src/components/UI/LoadingSpiner.js
+++ src/components/UI/LoadingSpiner.js
@@ -1,6 +1,19 @@
+import Backdrop from "../UI/Backdrop";
+
 import classes from "./LoadingSpinner.module.css";
 
-const LoadingSpinner = () => {
+const LoadingSpinner = (props) => {
+  if (props.overlay) {
+    return (
+      <>
+        <div className={classes.wrapper}>
+          <div className={`${classes.spinner} ${classes.overlay}`} />
+        </div>
+        <Backdrop dark />
+      </>
+    );
+  }
+
   return (
     <div className={classes.wrapper}>
       <div className={classes.spinner} />
